78d978b436e289e9042743c01ad4e1b69c4718de
John Mwaura <johnmwaura08@gmail.com>
2019-12-23 21:53:12 -0600

    added new updates

--- client/src/Components/Navbarr.js
+++ client/src/Components/Navbarr.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { Link as RRNavLink, BrowserRouter as Router} from 'react-router-dom'
-import Projects from './Projects'
+import React, { useState } from "react";
+import { Link as RRNavLink, BrowserRouter as Router, Switch } from "react-router-dom";
 
 
-import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
 import {
   Collapse,
   Navbar,
@@ -17,46 +16,70 @@ import {
   DropdownMenu,
   DropdownItem,
   NavbarText
-} from 'reactstrap';
-import Projects from './Bugs';
+} from "reactstrap";
 
-const Navbarr = (props) => {
+
+const Navbarr = props => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar  style={{backgroundColor: '#39ff14', alignItems: 'center', display: 'flex'}} light expand="lg">
-        <NavbarBrand className="mr-7" href="/">BUGZY</NavbarBrand>
+      <Navbar
+        style={{
+          backgroundColor: "#39ff14",
+          alignItems: "center",
+          display: "flex"
+        }}
+        light
+        expand="lg"
+      >
+        <NavbarBrand className="mr-7" href="/">
+          BUGZY
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-            <NavItem >
+            <NavItem>
               <NavLink href="/components/">HOME</NavLink>
             </NavItem>
-           
-            <UncontrolledDropdown nav inNavbar >
-              <DropdownToggle nav caret className='mr-6'>
+
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret className="mr-6">
                 DASHBOARD
               </DropdownToggle>
-              <DropdownMenu right className='nav-links'>
-                <Router>
-                {/* <IndexLinkContainer exact to ="/projects" > */}
-                <DropdownItem tag={(props) => <RRNavLink to="/projects" {...props} component={Projects}/>} >
-                  ADD NEW PROJECT
-                </DropdownItem>
-                {/* </IndexLinkContainer> */}
-               
-                </Router>
+              <DropdownMenu right className="nav-links">
+           
+                
+                      {/* <IndexLinkContainer exact to ="/projects" > */}
+                  <DropdownItem
+                    tag={props => (
+                      <RRNavLink
+                        to="/projects/new"
+                        {...props}
+                       
+                      />
+                    )}
+                  >
+                    ADD NEW PROJECT
+                  </DropdownItem>
+                  {/* </IndexLinkContainer> */}
+                 
+                
               
-                <DropdownItem >
-                  ADD NEW BUG
-                </DropdownItem>
+
+                <DropdownItem
+                 tag={props => (
+                  <RRNavLink
+                    to="/bugs/new"
+                    {...props}
+                   
+                  />
+                )}
+                >ADD NEW BUG</DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
-                  ADD NEW USER
-                </DropdownItem>
+                <DropdownItem>ADD NEW USER</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
 
@@ -65,25 +88,18 @@ const Navbarr = (props) => {
                 REPORTS
               </DropdownToggle>
               <DropdownMenu right>
-                <DropdownItem>
-                  VIEW PROJECTS
-                </DropdownItem>
-                <DropdownItem>
-                  VIEW BUGS
-                </DropdownItem>
+                <DropdownItem>VIEW PROJECTS</DropdownItem>
+                <DropdownItem>VIEW BUGS</DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
-                  VIEW USERS
-                </DropdownItem>
+                <DropdownItem>VIEW USERS</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
-           
           </Nav>
           <NavbarText>LOG OUT</NavbarText>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Navbarr;
