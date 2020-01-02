b619b8d10be1f776d8efb0dfcdc9cf2c72a91f99
John Mwaura <johnmwaura08@gmail.com>
2020-01-02 02:44:17 -0600

    added route for bugs

---
+++ server/routes/bugs.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+let Bug = require('../models/bug')
+/* GET bug listing. */
+router.route('/').get((req,res)=>{
+    Bug.find()
+        .then(bugs => res.json(bugs))
+        .catch(err => res.status(400).json("Error:" + err));
+
+})
+
+
+
+//post bugs to database
+
+
+router.route('/add').post((req,res)=>{
+
+const title = req.body.title;
+const project = req.body.project;
+const deadline = Date.parse(req.body.deadline);
+const bugtype = req.body.bugtype;
+const status = req.body.status;
+const comments = req.body.comments
+
+
+
+
+
+
+
+
+  const newBug = new Bug({
+
+    title,
+    project,
+    deadline,
+    bugtype,
+    status,
+    comments
+
+  });
+
+  newBug.save()
+        .then(() => res.json("Bug Added"))
+        .catch(err => res.status(400).json("Error:" + err));
+
+
+  console.log(newBug)
+
+
+})
+
+module.exports = router;
