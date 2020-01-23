cb0441947f884c389fc84f73cb1e6b6f35561e56
johnmwaura08 <johnmwaura08@gmail.com>
2020-01-22 21:28:45 -0600

    created reports component for users

---
+++ client/src/Components/UserReports.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import UsersTable from './UsersTable'
+
+
+export default class UserReports extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      users_array: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:6050/users/')
+      .then(res => {
+        this.setState({
+          users_array : res.data
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  DataTable() {
+    return this.state.projects_array.map((res, i) => {
+      return <UsersTable obj={res} number={i} key={i} />;
+    });
+  }
+
+
+  render() {
+    return (<div className="table-wrapper">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th> #</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>OCCUPATION</th>
+            <th>CONTACT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.DataTable()}
+        </tbody>
+      </Table>
+    </div>);
+  }
+}
