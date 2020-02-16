1b71732cad780a26c67edf4354b3482e2429fae5
johnmwaura08 <johnmwaura08@gmail.com>
2020-02-15 21:22:51 -0600

    added link to icon and new table data

--- client/src/Components/ProjectsTable.js
+++ client/src/Components/ProjectsTable.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import Button from 'react-bootstrap/Button';
+
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default class ProjectsTable extends Component {
     render() {
@@ -12,8 +18,14 @@ export default class ProjectsTable extends Component {
                 <td>{this.props.obj.title}</td>
                 <td>{this.props.obj.frontEnd}</td>
                 <td>{this.props.obj.backEnd}</td>
-                <td>
-                </td>
+                <td><FontAwesomeIcon icon ={faEdit} /> <br/>
+                <Link to={"/delete/"+this.props.obj._id}>
+                 <FontAwesomeIcon icon ={faTrash} onClick={() => {this.props.deleteProject(this.props.obj._id) }}/>
+                </Link>
+                 </td>
+
+                
+                
             </tr>
         );
     }
