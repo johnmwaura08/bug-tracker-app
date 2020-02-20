dd2bb575e8b1c2c333fe23df65586066a5bd9197
johnmwaura08 <johnmwaura08@gmail.com>
2020-02-20 04:47:27 -0600

    cleaned the code

---
+++ client/src/Components/UsersFolders/UsersTable.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class UsersTable extends Component {
+    render() {
+        console.log(this.props.key)
+        return (
+            <tr>
+                <td> {this.props.number + 1}</td>
+                
+                <td>{this.props.obj.username}</td>
+                <td>{this.props.obj.email}</td>
+                <td>{this.props.obj.occupation}</td>
+                <td>{this.props.obj.contact}</td>
+
+                <td>
+                </td>
+            </tr>
+        );
+    }
+}
